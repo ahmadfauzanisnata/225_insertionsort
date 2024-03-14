d6dd98c5030e0cebe4c3b0c99d58557ebf138a2b
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-03-14 15:25:49 +0700

    membuat insertion sort

--- prak 4/prak 4.cpp
+++ prak 4/prak 4.cpp
@@ -1,21 +1,27 @@
 // prak 4.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
 using namespace std;
+
 void insertionsort() {
     int i, n, j, temp;
     int arr[40];
-   void insertionsort() {
-        int i, n, j, temp;
-        int arr[40];
 
-        for (i = 1; i <= n - 1; i++) { //step 1
-            temp = arr[i]; //step2
+    for (i = 1; i <= n - 1; i++) { //step 1
+        temp = arr[i]; //step2
 
+        j = i - 1; //step 3
 
+        while (j >= 0 && arr[j] > temp) //step4
+        {
+            arr[j + 1] = arr[j]; //step 4a
+            j--;
+            arr[j + 1] = temp; //step 5
         }
+
     }
+}
+   
 
             
 
