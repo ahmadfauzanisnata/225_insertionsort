ae22718aa56eeb237197fdbc665490fda7030db0
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-03-14 15:05:40 +0700

    menambahkan variabel

--- prak 4/prak 4.cpp
+++ prak 4/prak 4.cpp
@@ -3,6 +3,14 @@
 
 #include <iostream>
 using namespace std;
+void insertionsort() {
+    int i, n, j, temp;
+    int arr[40];
+
+    for (i = 1; i <= n - 1; i++) { //step 1
+
+  
+    
 int main()
 
 {
