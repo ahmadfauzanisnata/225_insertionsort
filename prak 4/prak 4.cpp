22d06d8476992d5d956045385b278fc9aab2933c
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-03-14 15:32:30 +0700

    membuat input

--- prak 4/prak 4.cpp
+++ prak 4/prak 4.cpp
@@ -1,35 +1,26 @@
 // prak 4.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-void insertionsort() {
-    int i, n, j, temp;
-    int arr[40];
+int i, j, n, temp;
+int arr[20];
 
-    for (i = 1; i <= n - 1; i++) { //step 1
-        temp = arr[i]; //step2
-
-        j = i - 1; //step 3
-
-        while (j >= 0 && arr[j] > temp) //step4
-        {
-            arr[j + 1] = arr[j]; //step 4a
-            j--;
-            arr[j + 1] = temp; //step 5
+void input() {
+    while (true) {
+        cout << "masukan banyaknya elemen array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 eleman.\n";
         }
-
     }
-}
-   
-
-            
-
-
-  
-    
-int main()
-
-{
-    std::cout << "Hello World!\n";
-}
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "masukan elemen array" << endl;
+    cout << "====================" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
